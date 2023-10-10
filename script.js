e84e2d77072208fcf757e0bd5e81d111742a7607
Dharan7635 <dharanitharan1477@gmail.com>
2023-10-10 20:59:47 +0530

    Working As Expected

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const wrapper = document.querySelector(".wrapper"),
     inputPart = wrapper.querySelector(".input-part"),
     infoTxt = inputPart.querySelector(".info-text"),
     inputField = inputPart.querySelector("input"),
-    locationBtn = inputPart.querySelector("button");
+    locationBtn = inputPart.querySelector("button"),
+    wIcon = wrapper.querySelector(".weather-part img"),
+    back = wrapper.querySelector("header i");
+console.log(back);
 
 const apiKey = "51bbf56af29d4a526fde863ebefe832d";
 let api;
@@ -23,7 +26,7 @@ locationBtn.addEventListener("click", () => {
 
 function onSuccess(position) {
     const { latitude, longitude } = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
     fetchData();
 }
 
@@ -33,7 +36,7 @@ function onError(error) {
 }
 
 function requestApi(city) {
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     fetchData();
 }
 
@@ -46,12 +49,42 @@ function fetchData() {
 
 
 function weatherDetails(info) {
-    if(info.cod == "404"){
+    if (info.cod == "404") {
         infoTxt.innerText = `${inputField.value} isn't a valid city`;
-        infoTxt.classList.replace("pending","error");
+        infoTxt.classList.replace("pending", "error");
     }
-    else{
-        infoTxt.classList.remove("pending","error");
+    else {
+
+        const city = info.name;
+        const country = info.sys.country;
+        const { description, id } = info.weather[0];
+        const { feels_like, humidity, temp } = info.main;
+
+        if (id == 800) {
+            wIcon.src = "assets/clear.svg";
+        } else if (id >= 200 && id <= 232) {
+            wIcon.src = "assets/strom.svg";
+        } else if (id >= 600 && id <= 622) {
+            wIcon.src = "assets/snow.svg";
+        } else if (id >= 701 && id <= 781) {
+            wIcon.src = "assets/haze.svg";
+        } else if (id >= 801 && id <= 804) {
+            wIcon.src = "assets/cloud.svg";
+        } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
+            wIcon.src = "assets/rain.svg";
+        }
+
+        wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+        wrapper.querySelector(".weather").innerText = description;
+        wrapper.querySelector(".location span").innerText = `${city},${country}`;
+        wrapper.querySelector(".temp .numb-2").innerText = Math.floor(temp);
+        wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+
+        infoTxt.classList.remove("pending", "error");
         wrapper.classList.add("active");
+        console.log(info);
     }
 }
+back.addEventListener("click",()=>{
+    location.reload();
+});
